812a88eb8d0b2de6d71ac4f024dce3448d85bc93
Ananda Nur Hisanah <nurhisanah@gmail.com>
2023-06-08 11:51:25 +0800

    build: database connection

---
+++ database/db.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"see-weather-on-your-schedule/entity"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	db  *gorm.DB
+	err error
+)
+
+func StartDB() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	host := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	dbName := os.Getenv("DB_NAME")
+	password := os.Getenv("DB_PASSWORD")
+
+	config := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbName, dbPort)
+	dsn := config
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("error connecting to database:", err)
+	}
+
+	fmt.Println("success connecting to database")
+	db.Debug().AutoMigrate(entity.User{})
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
