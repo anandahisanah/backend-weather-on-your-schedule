03b4d8f1a55ef97a4bf42f737842b669af52e1c0
Ananda Nur Hisanah <nurhisanah@gmail.com>
2023-06-10 22:43:54 +0800

    feat: ununique city name

--- models/city.go
+++ models/city.go
@@ -6,7 +6,7 @@ type City struct {
 	ID         uint       `gorm:"primaryKey;autoIncrement" json:"id"`
 	ProvinceID int        `gorm:"not null" json:"province_id" valid:"required~province_id is required"`
 	Province   Province   `gorm:"foreignKey:ProvinceID" json:"province"`
-	Name       string     `gorm:"not null;unique" json:"name" valid:"required~name is required"`
+	Name       string     `gorm:"not null" json:"name" valid:"required~name is required"`
 	CreatedAt  *time.Time `json:"created_at"`
 	UpdatedAt  *time.Time `json:"updated_at"`
 	DeletedAt  *time.Time `json:"deleted_at"`
