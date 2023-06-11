9853b45d6650afa9a5bc81c055f25db57a4877da
Ananda Nur Hisanah <nurhisanah@gmail.com>
2023-06-12 01:10:48 +0800

    feat: relation model city and province

--- models/provinces.go
+++ models/provinces.go
@@ -7,6 +7,8 @@ type Province struct {
 	Code      string     `gorm:"not null;unique" json:"code" valid:"required~code is required"`
 	Name      string     `gorm:"not null;unique" json:"name" valid:"required~name is required"`
 	Endpoint  string     `gorm:"not null;unique" json:"endpoint" valid:"required~endpoint is required"`
+	Cities    []City     `gorm:"foreignKey:ProvinceID" json:"cities"`
+	Users     []User     `gorm:"foreignKey:ProvinceID" json:"users"`
 	CreatedAt *time.Time `json:"created_at"`
 	UpdatedAt *time.Time `json:"updated_at"`
 	DeletedAt *time.Time `json:"deleted_at"`
