a8f11228061d3b2fde5314f6f5fafbdf86256ff7
Ananda Nur Hisanah <nurhisanah@gmail.com>
2023-06-12 00:34:17 +0800

    build: seeder provinces and cities

--- main.go
+++ main.go
@@ -1,27 +1,122 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"see-weather-on-your-schedule/database"
-	"see-weather-on-your-schedule/routers"
+	"see-weather-on-your-schedule/models"
+	"see-weather-on-your-schedule/service"
+	"strings"
 )
+
+type FormattedForecast struct {
+	Areas []FormattedArea `json:"areas"`
+}
+
+type FormattedArea struct {
+	Name []Name `json:"name"`
+}
+
+type Name struct {
+	Language string `xml:"lang,attr" json:"language"`
+	Value    string `xml:",chardata" json:"value"`
+}
 
 func main() {
 	database.StartDB()
+	seederProvince()
+	seederCity()
 
-	r := routers.StartServer()
-	r.Run(":8080")
-
-	// regionSeeder()
+	// r := routers.StartServer()
+	// r.Run(":8080")
 }
 
-func regionSeeder() {
-	sqlFile, err := ioutil.ReadFile("./database/data/region.sql")
+func seederProvince() {
+	// truncate
+	err := database.GetDB().Exec("TRUNCATE TABLE provinces CASCADE").Error
 	if err != nil {
-		log.Fatal("Error reading SQL file:", err)
+		log.Fatal("Error truncating table:", err)
+	}
+	// find json file
+	filePath := "./database/data/provinces.json"
+	file, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatalln("Error opening JSON file:", err)
 	}
 
-	fmt.Println(string(sqlFile))
+	// parse json to slice of struct
+	var provinces []models.Province
+	err = json.Unmarshal(file, &provinces)
+	if err != nil {
+		log.Fatal("Error parsing JSON:", err)
+	}
+
+	for _, item := range provinces {
+		code := strings.Replace(item.Name, " ", "", -1)
+		endpoint := fmt.Sprintf("https://data.bmkg.go.id/DataMKG/MEWS/DigitalForecast/DigitalForecast-%s.xml", code)
+
+		province := models.Province{
+			Code:     code,
+			Name:     item.Name,
+			Endpoint: endpoint,
+		}
+
+		// create
+		err := database.GetDB().Create(&province).Error
+		if err != nil {
+			log.Fatal("Error saving to database:", err)
+		}
+	}
+
+	fmt.Println("Seeding Province complete")
+}
+
+func seederCity() {
+	provinces := []models.Province{}
+	err := database.GetDB().Find(&provinces).Error
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for index, province := range provinces {
+		jsonData, err := service.GetCity(province.Code)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		// Decode the JSON data
+		var formattedForecast FormattedForecast
+		err = json.Unmarshal(jsonData, &formattedForecast)
+		if err != nil {
+			fmt.Println("Error decoding JSON:", err)
+			return
+		}
+
+		// Extract city names from the formatted forecast
+		cityNames := make([]string, 0)
+		for _, formattedArea := range formattedForecast.Areas {
+			for _, name := range formattedArea.Name {
+				cityNames = append(cityNames, name.Value)
+			}
+		}
+
+		// Save city names to the database
+		for _, cityName := range cityNames {
+			city := models.City{
+				ProvinceID: int(province.ID),
+				Name:       cityName,
+			}
+			err := database.GetDB().Create(&city).Error
+			if err != nil {
+				fmt.Println("Error saving to database:", err)
+				return
+			}
+		}
+
+		fmt.Printf("City names saved to the database for province: %d. %s\n", index+1, province.Name)
+	}
 }
