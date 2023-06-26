497960941042f1bedf7e2783585bf58051d23faf
Ananda Nur Hisanah <nurhisanah@gmail.com>
2023-06-27 00:51:02 +0800

    feat: model forecast

--- models/forecast.go
+++ models/forecast.go
@@ -3,17 +3,16 @@ package models
 import "time"
 
 type Forecast struct {
-	ID         uint       `gorm:"primaryKey;autoIncrement" json:"id"`
-	ProvinceID int        `gorm:"not null" json:"province_id" valid:"required~province_id is required"`
-	Province   Province   `gorm:"foreignKey:ProvinceID" json:"province"`
-	CityID     int        `gorm:"not null" json:"city_id" valid:"required~city_id is required"`
-	City       City       `gorm:"foreignKey:CityID" json:"city"`
-	Datetime   *time.Time `gorm:"not null" json:"datetime" valid:"required~datetime is required"`
-	Weather    string     `gorm:"not null" json:"weather" valid:"required~weather is required"`
-	Raining    string     `gorm:"not null" json:"raining" valid:"required~raining is required"`
-	Uv         string     `gorm:"not null" json:"uv" valid:"required~uv is required"`
-	Wind       string     `gorm:"not null" json:"wind" valid:"required~wind is required"`
-	CreatedAt  *time.Time `json:"created_at"`
-	UpdatedAt  *time.Time `json:"updated_at"`
-	DeletedAt  *time.Time `json:"deleted_at"`
+	ID          uint       `gorm:"primaryKey;autoIncrement" json:"id"`
+	ProvinceID  int        `gorm:"not null" json:"province_id" valid:"required~province_id is required"`
+	Province    Province   `gorm:"foreignKey:ProvinceID" json:"province"`
+	CityID      int        `gorm:"not null" json:"city_id" valid:"required~city_id is required"`
+	City        City       `gorm:"foreignKey:CityID" json:"city"`
+	Datetime    *time.Time `gorm:"not null" json:"datetime" valid:"required~datetime is required"`
+	Humidity    string     `gorm:"not null" json:"humidity" valid:"required~humidity is required"`
+	WindSpeed   string     `gorm:"not null" json:"wind_speed" valid:"required~wind_speed is required"`
+	Temperature string     `gorm:"not null" json:"temperature" valid:"required~temperature is required"`
+	CreatedAt   *time.Time `json:"created_at"`
+	UpdatedAt   *time.Time `json:"updated_at"`
+	DeletedAt   *time.Time `json:"deleted_at"`
 }
